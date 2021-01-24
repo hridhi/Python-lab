954ef46dc2de0eb506c0e0c58f75fa0b8b67c8cf
hridhi <57177761+hridhi@users.noreply.github.com>
2021-01-24 12:30:38 +0530

    Create 3D-objects.py

---
+++ vpython/3D-objects.py
@@ -0,0 +1,7 @@
+GlowScript 3.0 VPython
+#different 3D objects
+arrow(pos=vec(0,0,0),color=color.red,axis=vec(1,1,1),shaftwidth=0,length=2.5)
+mybox = box(pos=vec(5,0,0),size=vec(1.5,1.5,1.5))
+sphere(pos=vec(-5,0,0),radius=0.5,color=color.green)
+cone(pos=vector(0,5,0),axis=vector(4,1,1),color=color.blue)
+points(pos=[vector(-5,0,5)], radius=5.5,color=color.yellow)
